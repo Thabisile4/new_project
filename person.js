c815f2334ae2af244edfac30723577112f0e0549
DA_MAC_160 <keswausiphile@gmail.com>
2025-02-10 16:22:32 +0200

    Update person.js with new functionality

--- person.js
+++ person.js
@@ -1,15 +1,23 @@
 class Person {
     constructor({ name, age, gender, interests }) {
-      this.name = name;
-      this.age = age;
-      this.gender = gender;
-      this.interests = interests;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.interests = interests;
     }
-  
+
     hello() {
-      return `Hello, my name is ${this.name}, I am ${this.age} years old, and I am interested in ${this.interests.join(", ")}.`;
+        return `Hello, my name is ${this.name}. I am ${this.age} years old, identify as ${this.gender}, and I enjoy ${this.interests.join(", ")}.`;
     }
-  }
-  
-  export default Person;
-  
+}
+
+// Example usage
+const person = new Person({
+    name: "Ryan",
+    age: 30,
+    gender: "male",
+    interests: ["being a hardarse", "agile", "SSD hard drives"],
+});
+
+const greeting = person.hello();
+console.log(greeting);
