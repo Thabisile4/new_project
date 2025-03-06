144a3b9b959a54ded8a16b4f3f61d60b3ce053cd
DA_MAC_160 <keswausiphile@gmail.com>
2025-03-06 13:46:19 +0200

    Add Person class with greeting and interest formatting

--- person.js
+++ person.js
@@ -1,31 +1,31 @@
 class Person {
-    constructor(name, age, gender, interests) {
-      this.name = name;
-      this.age = age;
-      this.gender = gender;
-      this.interests = interests;
-    }
-  
-    formatInterests() {
-      const { interests } = this;
-  
-      if (interests.length === 0) {
-        return "I have no interests.";
-      }
-      if (interests.length === 1) {
-        return `My interest is ${interests[0]}.`;
-      }
-      if (interests.length === 2) {
-        return `My interests are ${interests[0]} and ${interests[1]}.`;
-      }
-  
-      return `My interests are ${interests.slice(0, -1).join(", ")} and ${interests[interests.length - 1]}.`;
-    }
-  
-    hello() {
-      const interestsFormatted = this.formatInterests();
-      return `Hello, my name is ${this.name}, my gender is ${this.gender} and I am ${this.age} years old. ${interestsFormatted}`;
-    }
+  constructor(name, age, gender, interests) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
   }
-  
-  module.exports = {Person};
+
+  formatInterests() {
+    const { interests } = this;
+
+    if (interests.length === 0) {
+      return "I have no interests.";
+    }
+    if (interests.length === 1) {
+      return `My interest is ${interests[0]}.`;
+    }
+    if (interests.length === 2) {
+      return `My interests are ${interests[0]} and ${interests[1]}.`;
+    }
+
+    return `My interests are ${interests.slice(0, -1).join(", ")} and ${interests[interests.length - 1]}.`;
+  }
+
+  hello() {
+    const interestsFormatted = this.formatInterests();
+    return `Hello, my name is ${this.name}, my gender is ${this.gender} and I am ${this.age} years old. ${interestsFormatted}`;
+  }
+}
+
+module.exports = { Person };
