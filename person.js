e4f673d4765f9e201fdb10ce7fb383e5355f5538
DA_MAC_160 <keswausiphile@gmail.com>
2025-03-19 14:46:55 +0200

    Update functionality in person.js

--- person.js
+++ person.js
@@ -1,9 +1,31 @@
-const person = new Person({
-  name: "Ryan",
-  age: 30,
-  gender: "male",
-  interests: ["being a hardarse", "agile", "SSD hard drives"],
-});
+class Person {
+  constructor(name = 'Unknown', age = 0, gender = 'Not specified', interests = []) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+  }
 
-const greeting = person.hello();
-console.log(greeting);
+  formatInterests() {
+    const { interests } = this;
+
+    if (interests.length === 0) {
+      return "I have no interests.";
+    }
+    if (interests.length === 1) {
+      return `My interest is ${interests[0]}.`;
+    }
+    if (interests.length === 2) {
+      return `My interests are ${interests[0]} and ${interests[1]}.`;
+    }
+
+    return `My interests are ${interests.slice(0, -1).join(", ")} and ${interests[interests.length - 1]}.`;
+  }
+
+  hello() {
+    const interestsFormatted = this.formatInterests();
+    return `Hello, my name is ${this.name}, my gender is ${this.gender} and I am ${this.age} years old. ${interestsFormatted}`;
+  }
+}
+
+module.exports = { Person };
