cdbaed29dc37fde5c0dfc6f725f706b91d599960
DA_MAC_160 <keswausiphile@gmail.com>
2025-03-19 13:00:47 +0200

    Add new directory and modify person.js

--- person.js
+++ person.js
@@ -1,31 +1,9 @@
-class Person {
-  constructor(name, age, gender, interests) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.interests = interests;
-  }
+const person = new Person({
+  name: "Ryan",
+  age: 30,
+  gender: "male",
+  interests: ["being a hardarse", "agile", "SSD hard drives"],
+});
 
-  formatInterests() {
-    const { interests } = this;
-
-    if (interests.length === 0) {
-      return "I have no interests.";
-    }
-    if (interests.length === 1) {
-      return `My interest is ${interests[0]}.`;
-    }
-    if (interests.length === 2) {
-      return `My interests are ${interests[0]} and ${interests[1]}.`;
-    }
-
-    return `My interests are ${interests.slice(0, -1).join(", ")} and ${interests[interests.length - 1]}.`;
-  }
-
-  hello() {
-    const interestsFormatted = this.formatInterests();
-    return `Hello, my name is ${this.name}, my gender is ${this.gender} and I am ${this.age} years old. ${interestsFormatted}`;
-  }
-}
-
-module.exports = { Person };
+const greeting = person.hello();
+console.log(greeting);
